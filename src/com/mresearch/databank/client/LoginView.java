60086e74a2c2789dfdc47215a1a12628eac7891c
Ihor Reshetnov <reshet.ukr@gmail.com>
2014-12-07 18:56:46 +0200

    Add new oauth clent-id for national data bank.

--- src/com/mresearch/databank/client/LoginView.java
+++ src/com/mresearch/databank/client/LoginView.java
@@ -62,7 +62,7 @@ public class LoginView extends Composite {
 
   // This app's personal client ID assigned by the Google APIs Console
   // (http://code.google.com/apis/console).
- 	private static final String GOOGLE_CLIENT_ID = "157936524178.apps.googleusercontent.com";
+ 	private static final String GOOGLE_CLIENT_ID = "157936524178-6na0469evoo8vbucbc953fl82s6u0rch.apps.googleusercontent.com";
 
   // The auth scope being requested. This scope will allow the application to
   // identify who the authenticated user is.
